44a79734e2e1956271cc74ffdc6c2761e3213e57
davidkatz <15Dkatz@shcp.edu>
2016-09-10 09:39:41 -0400

    add / to the photo.jpeg path

--- index.js
+++ index.js
@@ -17,21 +17,14 @@ app.post('/convert', function(req, res, next) {
   console.log('convert the stuff!');
   console.log('req', req);
   var baseImg = `data:image/jpeg;base64,${req.body.base64}`
-  // console.log('baseImg', baseImg);
-
-  // var data = fs.readFileSync('base64', 'utf8');
   var base64Data = req.body.base64;
   base64Data += base64Data.replace('+', ' ');
 
   var binaryData = new Buffer(base64Data, 'base64').toString('binary');
 
-  require('fs').writeFile('photo.jpeg', binaryData, 'binary', function(err) {
+  require('fs').writeFile('/photo.jpeg', binaryData, 'binary', function(err) {
     console.log(err);
   })
-  // console.log('req.connection.ReadableState.buffer', req.connection.ReadableState.buffer);
-  // console.log('res', res);
-  // var tempPath = req.files.file.path;
-  // console.log('tempPath', tempPath);
   res.send('done');
 })
 
