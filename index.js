3f1359c2925ca401a36bd3599941c1c1c2b56cdd
davidkatz <15Dkatz@shcp.edu>
2016-09-10 12:44:49 -0400

    change to 0.0.0.0 for IP

--- index.js
+++ index.js
@@ -75,6 +75,6 @@ app.post('/convert', function(req, res, next) {
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || '0.0.0.0', function(){
   console.log(`Express server listening on port ${this.address().port} in ${app.settings.env} mode`);
 });
