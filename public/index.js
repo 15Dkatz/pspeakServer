b3b719cc918cd79fcab64c013ac2f46ee285df0a
davidkatz <15Dkatz@shcp.edu>
2016-09-10 21:54:40 -0400

    heroku update

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var tesseract = require('node-tesseract');
 var firebase = require('firebase');
 var Translate = require('@google-cloud/translate');
-var ISO6391 = require('iso-639-1');
+// var ISO6391 = require('iso-639-1');
 
 var config = {
     apiKey: "AIzaSyCa1TSayY_Fqn9nrTXU8WqVwQSjdBF5haQ",
@@ -59,56 +59,56 @@ app.post('/convert', function(req, res, next) {
 
   res.send('done');
 })
-
-app.post('/translate', function(req, res, next) {
-  // translate endpoint!
-  // use the req to recognize what language you need to translate to
-  // use req to eventually recognize the language you want to translate FROM
-  textRef.once('value', function(data) {
-    let text = data.val().original_text;
-    console.log('translate this text', text);
-    let language = req.body.language;
-    let voice = '';
-    // switch Spanish, French, German, Tagalog, Mandarin
-    // translate language into code
-    if (language == 'spanish') {
-      language = 'es'
-    }
-    if (language == 'french') {
-      language = 'fr'
-    }
-    if (language == 'german') {
-      language = 'de'
-    }
-    if (language == 'filipino') {
-      language = 'tl'
-    }
-
-    console.log('into this language', language);
-
-    let translate = Translate({
-      key: 'AIzaSyDXZS-_PJySN03yrO64Y-qCdiUtklFRblY'
-    })
-
-    // english = 'en'
-    translate.translate(text, {
-      from: 'en',
-      to: language
-    }, function(err, translation, apiResponse) {
-      if (err) {
-        console.log('err', err);
-      }
-
-      console.log('Translate to %s:', ISO6391.getName(language));
-      console.log('translation', translation);
-
-      // if used one more time, make a helper method for duplicate code.
-      textRef.update({original_text: text});
-      textRef.update({text: translation});
-    })
-  })
-  // translate the text from firebase and resave it as the translated text
-});
+// 
+// app.post('/translate', function(req, res, next) {
+//   // translate endpoint!
+//   // use the req to recognize what language you need to translate to
+//   // use req to eventually recognize the language you want to translate FROM
+//   textRef.once('value', function(data) {
+//     let text = data.val().original_text;
+//     console.log('translate this text', text);
+//     let language = req.body.language;
+//     let voice = '';
+//     // switch Spanish, French, German, Tagalog, Mandarin
+//     // translate language into code
+//     if (language == 'spanish') {
+//       language = 'es'
+//     }
+//     if (language == 'french') {
+//       language = 'fr'
+//     }
+//     if (language == 'german') {
+//       language = 'de'
+//     }
+//     if (language == 'filipino') {
+//       language = 'tl'
+//     }
+//
+//     console.log('into this language', language);
+//
+//     let translate = Translate({
+//       key: 'AIzaSyDXZS-_PJySN03yrO64Y-qCdiUtklFRblY'
+//     })
+//
+//     // english = 'en'
+//     translate.translate(text, {
+//       from: 'en',
+//       to: language
+//     }, function(err, translation, apiResponse) {
+//       if (err) {
+//         console.log('err', err);
+//       }
+//
+//       // // // console.log('Translate to %s:', ISO6391.getName(language));
+//       console.log('translation', translation);
+//
+//       // if used one more time, make a helper method for duplicate code.
+//       textRef.update({original_text: text});
+//       textRef.update({text: translation});
+//     })
+//   })
+//   // translate the text from firebase and resave it as the translated text
+// });
 
 
 
